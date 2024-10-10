4e70a14b2ef77cec469ebb2f25c73ca3676b87bc
khireddinemahaline <khirozr@gmail.com>
2024-10-10 16:37:35 -0400

    mixed typin

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -2,10 +2,10 @@
 """sum_mixed_list"""
 
 
-from typing import Union
+from typing import Union, List
 
 
-def sum_mixed_list(mxd_lst: Union[int, float]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """miced value in list"""
     sum: float = 0
     for i in mxd_lst:
