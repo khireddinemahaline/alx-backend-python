7ff48a80e61b7ec02442c64fa8e5e31038fccb56
khireddinemahaline <khirozr@gmail.com>
2024-10-10 16:55:54 -0400

    mixed type

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 """sum_mixed_list"""
 
-from typing import Union
+from typing import Union, Tuple
 
 
-def to_kv(k: str, v: Union[int, float]) -> tuple[str, Union[str, float]]:
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """"""
     return (k, float(v ** 2))
