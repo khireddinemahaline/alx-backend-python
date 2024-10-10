17567109b27ec59e679f24a74c17b4e73780ece5
khireddinemahaline <khirozr@gmail.com>
2024-10-10 16:22:48 -0400

    annonation

--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-"""concat two string"""
+"""flot to str"""
 
 
 def to_str(n: float) -> str:
     """
-    concat str1 + str2
+    implicit conv n:float -> str
     """
     return str(n)
