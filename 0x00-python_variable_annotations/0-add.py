a167b266082c0b0895b2f6bc1f15614e96238998
khireddinemahaline <khirozr@gmail.com>
2024-10-10 16:00:20 -0400

    add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+""" add two float number """
+
+
+def add(a: float, b: float):
+    """
+    a: float
+    b: float
+    sum = a + b
+    """
+    sum: float = a + b
+    return sum
