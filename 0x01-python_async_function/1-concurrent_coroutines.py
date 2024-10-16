44a4c30340737d073922e93a6250018c7ac2c1a8
khireddinemahaline <khirozr@gmail.com>
2024-10-16 16:34:17 -0400

    asyn/await

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -4,7 +4,7 @@ async / await
 - use asyncio library : could manage create tasks
 """
 
-import random
+
 import asyncio
 from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
@@ -12,8 +12,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """return a List of float delat float values"""
-    float_list = []
-    for iter in range(n):
-        iter = await asyncio.create_task(wait_random(max_delay))
-        float_list.append(iter)
-    return float_list
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
