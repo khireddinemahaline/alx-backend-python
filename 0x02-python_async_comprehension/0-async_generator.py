d0297e0ca94ed1b3ac0b181c05d8d5c7ef89b66a
khireddinemahaline <khirozr@gmail.com>
2024-10-18 16:38:25 -0400

    async_comprehesion

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,14 +4,14 @@ create a generateur async
 """
 
 
-import asyncio
-import random
-from typing import AsyncGenerator
+
+from asyncio import sleep
+from random import uniform
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
-    """10 ele after 1 second"""
+async def async_generator() -> Generator[float, None, None]:
+    """ Async Generator """
     for _ in range(10):
-        await asyncio.sleep(1)
-
-        yield random.uniform(0, 10)
+        await sleep(1)
+        yield uniform(0, 10)
